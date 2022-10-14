52b648ad954e49bbe91d5dc46b3667137c1f8fcc
ramesh-sharma12 <115834160+ramesh-sharma12@users.noreply.github.com>
2022-10-15 01:03:19 +0530

    Code using arrays

---
+++ c1.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+ int a[2];
+ for(int i=0;i<=2;i++)
+ {
+ cin>>a[i];
+ }
+ for(int i=0;i<=2;i++)
+ {
+ cout<<a[i]<<endl;
+ }
+ return 0;
+}
